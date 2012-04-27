f6defa86ff30c5998e696cb4c6fd7b3382a3e3db
Scott Brooks <scottb@tsbrooks.com>
2012-04-27 12:36:52 -0400

    Initial checkin

---
+++ WcfService/CallCenter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WcfService
+{
+   public class CallCenter : ICallCenter
+   {
+      public BedDto GetBed(long bedId)
+      {
+         BedDto retVal = new BedDto();
+
+         retVal.Id = bedId;
+         retVal.Poc = "Id-" + bedId;
+
+         return retVal;
+      }
+   }
+}
---
+++ WcfService/Dto/BedDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WcfService
+{
+   public class BedDto
+   {
+      public long Id { get; set; }
+      public string Poc { get; set; }  // Point of Care
+   }
+}
